028ff918629a1c8c73b67d2a42a5fdc43e9ef037
AGChad <agchadbourne@wpi.edu>
2025-04-07 13:55:06 -0400

    added slider game

---
+++ Slider/app.js
@@ -0,0 +1,20 @@
+
+import PicturePuzzle from './PicturePuzzle.js'
+
+const picturePuzzle = new PicturePuzzle(
+    document.querySelectorAll('#puzzle-wrapper > div')[0],
+    '../solarpunk.png',
+    600
+);
+
+// const picturePuzzle2 = new PicturePuzzle(
+//     document.querySelectorAll('#puzzle-wrapper > div')[1],
+//     '../solarpunk.png',
+//     600,
+//     4
+// );
+
+
+picturePuzzle.onFinished = function() {
+    console.log("Good job");
+};
