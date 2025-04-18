166cb7346aebb8b520f24eb97a3b6be35b650545
Dennis Field <djfield@wpi.edu>
2025-04-18 00:35:42 -0400

    Basic setup for scroll
    Not fully function, needs:
    - Centerline observer
    - Scroll padding

--- Caesar/Caesar.js
+++ Caesar/Caesar.js
@@ -1,4 +1,16 @@
+window.onload = () => {
+    const pointerScroll = (elem) => {
+        const dragStart = (ev) => elem.setPointerCapture(ev.pointerId);
+        const dragEnd = (ev) => elem.releasePointerCapture(ev.pointerId);
+        const drag = (ev) => elem.hasPointerCapture(ev.pointerId) && (elem.scrollLeft -= ev.movementX);
 
+        elem.addEventListener("pointerdown", dragStart);
+        elem.addEventListener("pointerup", dragEnd);
+        elem.addEventListener("pointermove", drag);
+    }
+
+    document.querySelectorAll(".scroller").forEach(pointerScroll);
+}
 
 function letterShift(x, y, code) {
     let shift = x + y;
