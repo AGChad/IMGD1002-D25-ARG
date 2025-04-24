311f99e0a486a5b71f8bf73c8ee2d4d9c2978d77
AGChad <agchadbourne@wpi.edu>
2025-04-23 20:20:15 -0400

    added orders text and styled page better

--- Transmission/transmission.js
+++ Transmission/transmission.js
@@ -7,7 +7,7 @@ function checkPassword() {
         // Not whitespace, check for correctness.
         if (inputPassword.toLowerCase() === LOCATION_PASSWORD.toLowerCase()) {
             // Matched password!
-            window.location = "location-found-546392";
+            window.location.href = "location-found-546392.html";
         } else {
             // Wrong password, reset field.
             document.getElementById("location-password").value = "";
